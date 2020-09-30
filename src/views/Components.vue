e692c17e2d46b36c60b4a270d3a87324b9255c42
Josh Miranda <joshuamiranda@unomaha.edu>
2020-09-30 11:12:05 -0500

    Fix vuetify installation, set up components page, started nav bar, edited router

---
+++ src/views/Components.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="components">
+      <HelloWorld />
+      <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld'
+
+export default {
+  name: 'Components',
+  components: {
+    HelloWorld,
+  }
+}
+</script>
