c31be456a460d47847167c6f7ffebf4fb04334c4
Josh Miranda <joshuamiranda@unomaha.edu>
2020-10-08 13:53:59 -0500

    Completed Site Nav with mobile view, updated logo, completed routing to site pages, completed templates for all pages

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="contact">
+      <h1>This is the contact page</h1>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'About',
+  name: 'Contact',
   components: {
   }
 }
